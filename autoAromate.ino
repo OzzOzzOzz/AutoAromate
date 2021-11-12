cd718c9382046f9bc7dedb65e2d338aa8045ac61
Bisson Rémi <remi.bisson@epitech.eu>
2021-11-12 17:44:48 +0100

    [+] basic check function every 15sec

--- autoAromate.ino
+++ autoAromate.ino
@@ -1,39 +1,13 @@
-/*
-  -----------------------
-  ESPDASH - Basic Example
-  -----------------------
-
-  Skill Level: Intermediate
-
-  In this example we will be creating a basic dashboard which consists 
-  of some cards and then update them in realtime ( at 3s interval ).
-
-  Github: https://github.com/ayushsharma82/ESP-DASH
-  WiKi: https://ayushsharma82.github.io/ESP-DASH/
-
-  Works with both ESP8266 & ESP32
-*/
-
 #include <Arduino.h>
 #include "wifiConfig.h"
 #include "calibrationConfig.h"
-#if defined(ESP8266)
-  /* ESP8266 Dependencies */
-  #include <ESP8266WiFi.h>
-  #include <ESPAsyncTCP.h>
-  #include <ESPAsyncWebServer.h>
-#elif defined(ESP32)
-  /* ESP32 Dependencies */
-  #include <WiFi.h>
-  #include <AsyncTCP.h>
-  #include <ESPAsyncWebServer.h>
-#endif
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <ESPDash.h>
+#include <arduino-timer.h>
 
-
-/* Your WiFi Credentials */
-const char* ssid = _SSID_; // SSID
-const char* password = _PASSWORD_; // Password
+//  ============================
 
 /* Start Webserver */
 AsyncWebServer server(80);
@@ -41,13 +15,61 @@ AsyncWebServer server(80);
 /* Attach ESP-DASH to AsyncWebServer */
 ESPDash dashboard(&server); 
 
-/* 
-  Dashboard Cards 
-  Format - (Dashboard Instance, Card Type, Card Name, Card Symbol(optional) )
-*/
-Card light(&dashboard, GENERIC_CARD, "Light", "°%");
-Card humidity(&dashboard, HUMIDITY_CARD, "Humidity", "%");
+//  ============================
 
+Card lightDisplay(&dashboard, GENERIC_CARD, "Light", "%");
+Card humidityDisplay(&dashboard, HUMIDITY_CARD, "Humidity", "%");
+Card pumpSystemButton(&dashboard, BUTTON_CARD, "Pump system button");
+Card wantedHumidityLevelSlider(&dashboard, SLIDER_CARD, "Wanted humidity", "%", 20, 80);
+
+//  ============================
+
+bool pumpSystemActive = false;
+int pumpWateringDuration = _DEFAULT_PUMP_WATER_DURATION_;
+int wantedHumidityLevel = _DEFAULT_WANTED_HUMIDITY_LEVEL_;
+int humidityLevel = 0;
+
+//  ============================
+
+auto timer = timer_create_default();
+
+//  ============================
+
+uint16_t ReadLight() {
+  float value = 0;
+  digitalWrite(_LIGHT_PORT_, HIGH);
+  delay(100);
+  value = float(analogRead(A0));
+  digitalWrite(_LIGHT_PORT_, LOW);
+  return(((value - _MIN_LIGHT_VALUE_) / (_MAX_LIGHT_VALUE_ - _MIN_LIGHT_VALUE_)) * 100);
+}
+
+uint16_t ReadHumidity() {
+  float value = 0;
+  digitalWrite(_HUMIDITY_PORT_, HIGH);
+  delay(100);
+  value = float(analogRead(A0));
+  digitalWrite(_LIGHT_PORT_, LOW);
+  humidityLevel = ((value - _MIN_LIGHT_VALUE_) / (_MAX_LIGHT_VALUE_ - _MIN_LIGHT_VALUE_)) * 100;
+  return(humidityLevel);
+}
+
+void handlePumpSystem() {
+  humidityDisplay.update(ReadHumidity());
+  dashboard.sendUpdates();
+}
+
+void handleLightSystem() {
+  lightDisplay.update(ReadLight());
+  dashboard.sendUpdates();
+}
+
+bool check(void *) {
+  Serial.println("checking");
+  handlePumpSystem();
+  handleLightSystem();
+  return true;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -58,7 +80,7 @@ void setup() {
 
   /* Connect WiFi */
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(_SSID_, _PASSWORD_);
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
       Serial.printf("WiFi Failed!\n");
       return;
@@ -66,49 +88,25 @@ void setup() {
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  /* Start AsyncWebServer */
   server.begin();
-}
 
-//uint16_t analogReadPort(int port) {
-//  uint16_t value = 0;
-//  digitalWrite(port, HIGH);
-//  delay(100);
-//  value = analogRead(A0);
-//  digitalWrite(port, LOW);
-//  return(value);
-//}
+  pumpSystemButton.attachCallback([&](bool value){
+    pumpSystemActive = value;
+    Serial.println("pumpSystemButton: "+String((pumpSystemActive)?"true":"false"));
+    pumpSystemButton.update(pumpSystemActive);
+    dashboard.sendUpdates();
+  });
 
-uint16_t ReadLight() {
-  float value = 0;
-  digitalWrite(_LIGHT_PORT_, HIGH);
-  delay(100);
-  value = float(analogRead(A0));
-  digitalWrite(_LIGHT_PORT_, LOW);
-  return(((value - _MIN_LIGHT_VALUE) / (_MAX_LIGHT_VALUE - _MIN_LIGHT_VALUE)) * 100);
-}
+  wantedHumidityLevelSlider.attachCallback([&](int value) {
+    wantedHumidityLevel = value;
+    Serial.println("[wantedHumidityLevelSlider]: "+String(wantedHumidityLevel));
+    wantedHumidityLevelSlider.update(wantedHumidityLevel);
+    dashboard.sendUpdates();
+  });
 
-uint16_t ReadHumidity() {
-  float value = 0;
-  digitalWrite(_HUMIDITY_PORT_, HIGH);
-  delay(100);
-  value = float(analogRead(A0));
-  Serial.println(value);
-  digitalWrite(_LIGHT_PORT_, LOW);
-  return(((value - _MIN_LIGHT_VALUE) / (_MAX_LIGHT_VALUE - _MIN_LIGHT_VALUE)) * 100);
+  timer.every(15000, check);
 }
 
 void loop() {
-  /* Update Card Values */
-  light.update(ReadLight());
-  humidity.update(ReadHumidity());
-
-  /* Send Updates to our Dashboard (realtime) */
-  dashboard.sendUpdates();
-
-  /* 
-    Delay is just for demonstration purposes in this example,
-    Replace this code with 'millis interval' in your final project.
-  */
-  delay(200);
+  timer.tick();
 }
